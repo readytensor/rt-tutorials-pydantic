60eaa982858484d435c94cddde7eb2351f2f2611
Abu Desai <abhyuday.desai@gmail.com>
2023-03-03 22:41:47 -0800

    cleanup in pydantic data mode; docstrings and type hints

--- app/data_model/infer_data_model.py
+++ app/data_model/infer_data_model.py
@@ -1,47 +1,45 @@
-
-
-from typing import List
+from typing import List, Dict, Any
 from pydantic import BaseModel, validator
+from data_management.schema_provider import BinaryClassificationSchema
 
-
-def get_infer_request_model(schema: dict):
-    """ Returns appropriate pydantic model to verify input data for inference
-        using the given schema
+#-> BaseModel
+def get_infer_request_model(schema: BinaryClassificationSchema):
+    """ Returns Pydantic  model to verify input data for inference using the given schema
 
     Args:
-        schema (dict): schema for the binary classification problem
+        schema (BinaryClassificationSchema): schema for the binary classification problem
 
     Returns:
-        pydantic model: pydantic model to verify input data for inference
+        Pydantic model: Pydantic  model to verify input data for inference
     """
-
     class InferenceRequest(BaseModel): 
-        instances: List[dict]
+        instances: List[Dict[str, Any]]
 
         @validator("instances", pre=True, each_item=True)
-        def has_id_field(cls, v): 
-            ''' Check that each sample has the expected id field'''
-            assert schema.id_field in v.keys(), f"Required ID field '{schema.id_field}' missing in input sample {v}"
+        def has_id_field(cls, v: Dict[str, Any]) -> Dict[str, Any]:
+            ''' Check that each sample has the expected ID field'''
+            if schema.id_field not in v.keys():
+                raise ValueError(f"Required ID field '{schema.id_field}' missing in input sample: {v}")
             return v
 
         @validator("instances", pre=True, each_item=True)
-        def has_all_required_features(cls, v): 
+        def has_all_required_features(cls, v: Dict[str, Any]) -> Dict[str, Any]:
             ''' Check that each sample has all the required features'''
             keys_ = v.keys()
             for k in schema.features: 
-                assert k in keys_, f"Required feature '{k}' missing in input sample {v}"
+                if k not in keys_:
+                    raise ValueError(f"Required feature '{k}' missing in input sample: {v}")
             return v
 
         @validator("instances", pre=True, each_item=True)
-        def has_correct_data_types_for_features(cls, v): 
+        def has_correct_data_types_for_features(cls, v: Dict[str, Any]) -> Dict[str, Any]:
             ''' Check that each feature is of correct type'''    
             for f in schema.numeric_features: 
-                assert isinstance(v[f], (int, float)) or v[f] is None, f"Type error: Data type of feature {f} should be one of [int, float, NoneType]. Given value {v[f]} is of type {type(v[f])}"
-                    
+                if not isinstance(v[f], (int, float)) and v[f] is not None:
+                    raise TypeError(f"Type error: Data type of feature {f} should be one of [int, float] or value can be None. Given value {v[f]} is of type {type(v[f])}")                    
             for f in schema.categorical_features:  
-                # we check for str and int or float because the data may be encoded as int or float
-                assert isinstance(v[f], (str, int, float)) or v[f] is None, f"Type error: Data type of feature {f} should be one of [str, int, float, NoneType]. Given value {v[f]} is of type {type(v[f])}"               
-            
+                if not isinstance(v[f], (str, int, float)) and v[f] is not None:
+                    raise TypeError(f"Type error: Data type of feature {f} should be one of [str, int, float] or value can be None. Given value {v[f]} is of type {type(v[f])}")            
             return v
         
     return InferenceRequest
